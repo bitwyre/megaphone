976766650f2a6b04881cc2b3d2c2b93093c38faa
Alex Hultman <alexhultman@gmail.com>
2020-01-11 20:22:56 +0100

    Add submodules

---
+++ trades.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include "hiredis/hiredis.h"
+#include "hiredis/async.h"
+#include "hiredis/adapters/libuv.h"
+
+void onMessage(redisAsyncContext *c, void *reply, void *privdata) {
+    redisReply *r = reply;
+    if (reply == NULL) return;
+
+    if (r->type == REDIS_REPLY_ARRAY) {
+        for (int j = 0; j < r->elements; j++) {
+            printf("%u) %s\n", j, r->element[j]->str);
+        }
+    }
+}
+
+int main (int argc, char **argv) {
+    signal(SIGPIPE, SIG_IGN);
+    struct event_base *base = event_base_new();
+
+    redisAsyncContext *c = redisAsyncConnect("127.0.0.1", 6379);
+    if (c->err) {
+        printf("error: %s\n", c->errstr);
+        return 1;
+    }
+
+    redisLibeventAttach(c, base);
+    redisAsyncCommand(c, onMessage, NULL, "SUBSCRIBE testtopic");
+    event_base_dispatch(base);
+    return 0;
+}
