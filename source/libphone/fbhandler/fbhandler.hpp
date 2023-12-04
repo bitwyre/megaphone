3de915affc18d957244f398d55e9d670426bc5bf
SteveGremory <tejveer@bitwyre.com>
2023-12-04 18:26:16 +0000

    fix: added extra tests for depthL2 events

--- source/libphone/fbhandler/fbhandler.hpp
+++ source/libphone/fbhandler/fbhandler.hpp
@@ -38,8 +38,19 @@ public:
 
 		if constexpr (std::is_same_v<FlatBufType, Bitwyre::Flatbuffers::Depthl2::DepthEvent>) {
 			type_table = Bitwyre::Flatbuffers::Depthl2::DepthEventTypeTable();
-			if (Bitwyre::Flatbuffers::Depthl2::VerifyDepthEventBuffer(verifier))
+			if (Bitwyre::Flatbuffers::Depthl2::VerifyDepthEventBuffer(verifier)) {
+				// Extra checks for vectors and whatnot
+				auto data_flatbuf =
+					Bitwyre::Flatbuffers::Depthl2::GetDepthEvent(buf);
+				const auto& event_data = data_flatbuf->data();
+
+				if (event_data->asks()->size() <= 0)
+					return "Invalid DepthL2 event received, no asks.";
+				else if (event_data->bids()->size() <= 0)
+					return "Invalid DepthL2 event received, no bids.";
+
 				return flatbuffers::FlatBufferToString(buf, type_table);
+			}
 			else
 				return "Invalid buffer received, PLEASE REPORT THIS to the provider";
 
